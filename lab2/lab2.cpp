9a8f01f60b71d464f5b6da6abb34b18eef6587f8
dark fox <danyamatlaga63@gmail.com>
2023-10-10 13:25:07 +0300

    edit file

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 int main()
 {
-    //SetConsoleCP(1251);
-    //SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    //int m = 10, h = 5, v = 4;
-    //float e, p, g = 9.80665f;
+    int m = 10, h = 5, v = 4;
+    float e, p, g = 9.80665f;
 
-    //cout << "\t\t\tЗастосунок для обчислення кінетичної та потенціальної енергії\n";
-    //cout << "массa m = " << m << endl;
-    //cout << "висотa h = " << h << endl;
-    //cout << "швидкість v = " << v << endl << endl;
+    cout << "\t\t\tЗастосунок для обчислення кінетичної та потенціальної енергії\n";
+    cout << "массa m = " << m << endl;
+    cout << "висотa h = " << h << endl;
+    cout << "швидкість v = " << v << endl << endl;
 
-    //e = (m * pow(v, 2)) / 2;
-    //p = m * g * h;
+    e = (m * pow(v, 2)) / 2;
+    p = m * g * h;
 
-    //cout << "Кінетична енергія: " << e << "Дж\n";
-    //cout << "Потенційна енергія: " << p << "Дж\n";
+    cout << "Кінетична енергія: " << e << "Дж\n";
+    cout << "Потенційна енергія: " << p << "Дж\n";
 
 
       //SetConsoleCP(1251);
@@ -66,7 +66,7 @@ int main()
     printf("Кінетична енергія: %f Дж\n", e);
     printf("Потенційна енергія: %f Дж\n", p);*/
 
-      SetConsoleCP(1251);
+     /* SetConsoleCP(1251);
       SetConsoleOutputCP(1251);
 
       int m, h, v;
@@ -88,5 +88,5 @@ int main()
       p = m * g * h;
 
       printf("Кінетична енергія: %f Дж\n", e);
-      printf("Потенційна енергія: %f Дж\n", p);
+      printf("Потенційна енергія: %f Дж\n", p);*/
 }
